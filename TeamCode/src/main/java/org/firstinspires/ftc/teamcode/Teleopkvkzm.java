d20c6d9d9697287e8db4e5081c97472e13a20ae7
kvanklei <kaylavk@yahoo.com>
2024-11-04 20:41:53 -0600

    Adding pivot positions for both motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleopkvkzm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleopkvkzm.java
@@ -14,15 +14,23 @@ public class Teleopkvkzm extends LinearOpMode {
         DcMotor bl = hardwareMap.get(DcMotor.class, "bl");
         DcMotor br = hardwareMap.get(DcMotor.class, "br");
         DcMotor arm = hardwareMap.get(DcMotor.class, "arm");
+        DcMotor pivot_1 = hardwareMap.get(DcMotor.class,"pivot1");
+        DcMotor pivot_2 = hardwareMap.get(DcMotor.class,"pivot2");
 
 
-      //   bl.setDirection(DcMotorSimple.Direction.REVERSE);
-     //    br.setDirection(DcMotorSimple.Direction.REVERSE);
+      //bl.setDirection(DcMotorSimple.Direction.REVERSE);
+     //br.setDirection(DcMotorSimple.Direction.REVERSE);
         // positions for arm
         int p1 = 0;
         int p2 = -700;
         int p3 = -1400;
         int p4 = -2100;
+        int pivotp1 = 0;
+        int pivotp2 = -60;
+        int pivotp3 = -120;
+
+
+
 
         //resetting encoder back to 0
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -59,8 +67,38 @@ public class Teleopkvkzm extends LinearOpMode {
                 arm.setPower(0);
             }
 
+            //arm pivot
+            if (gamepad1.dpad_down) {
+                pivot_1.setTargetPosition(pivotp1);
+                pivot_1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                pivot_1.setPower(1);
+                pivot_2.setTargetPosition(pivotp1);
+                pivot_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                pivot_2.setPower(1);
+            } else if (gamepad1.dpad_right) {
+                pivot_1.setTargetPosition(pivotp2);
+                pivot_1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                pivot_1.setPower(1);
+                pivot_2.setTargetPosition(pivotp2);
+                pivot_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                pivot_2.setPower(1);
+
+            } else if (gamepad1.dpad_up) {
+                pivot_1.setTargetPosition(pivotp3);
+                pivot_1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                pivot_1.setPower(1);
+                pivot_2.setTargetPosition(pivotp3);
+                pivot_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                pivot_2.setPower(1);
+
+            } else {
+                pivot_1.setPower(0);
+                pivot_2.setPower(0);
+            }
         }
+
     }
+
 
 
 }
